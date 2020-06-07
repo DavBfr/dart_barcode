59b22189cf042a02dbd3b10e4a8df389f3d37a63
David PHAM-VAN <dev.nfet.net@gmail.com>
2020-06-07 11:08:03 -0400

    Improve demo

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,13 +5,13 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "index.html": "49024333c86d5f83b0283415f1013175",
 "/": "49024333c86d5f83b0283415f1013175",
-"main.dart.js": "3b721874e873c639c0bcca7cf2d72bbb",
+"main.dart.js": "9830dfcd06f8524a781a86634d1dfa03",
 "favicon.png": "c44f54d0521b7c14562539eab5da4565",
 "icons/Icon-192.png": "9cce240a8c959ee2d167b1ef3c36fae3",
 "icons/Icon-512.png": "8a4d078e9360e26d9029d9f9b270bdad",
 "manifest.json": "a72d3975920624ae930644d021e7ba7d",
 "assets/AssetManifest.json": "99914b932bd37a50b983c5e7c90ae93b",
-"assets/NOTICES": "2ec1a6647a7037fb3cbddeb0cffdf502",
+"assets/NOTICES": "d9a74caa2320e204c7be886bb9a36418",
 "assets/FontManifest.json": "580ff1a5d08679ded8fcf5c6848cece7",
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16"
 };
