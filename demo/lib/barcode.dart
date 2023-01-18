de50987a80fc71700da33a935a02d6b3ab7d7e2b
Tobias Eklund <tobias.eklund@sogeti.se>
2023-01-18 11:02:42 +0100

    Added sample string

--- demo/lib/barcode.dart
+++ demo/lib/barcode.dart
@@ -14,7 +14,10 @@
  * limitations under the License.
  */
 
+import 'dart:convert';
+
 import 'package:barcode_widget/barcode_widget.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -38,15 +41,18 @@ class BarcodeView extends StatelessWidget {
       return BarcodeError(message: error.message);
     }
 
+    final String sampleString = 'eNplkj-IE0EUxomW_gEFJYdwHhaC4CazMzvZZG1uD2PheSJEMBeRY-btJDcm2V12JktULHIWJxY2J1hvlFhdc7axsbCxExtLa73mGsXKSYgnXIaZx_B9vPd-b5ghi9ePjZ_VP74D1mmJag3TEoh-HGOEMaKI3kVlRB3aAMn0P9H5L8qAYxfaMuiLTa1j5RWLOmGhknqzx612ainNWjJsFVLW6-gCe9xLRCEUutgWj1SRR9ySSvVEYinZCkVSdB0BlNPAISXmoDKp2JzaFAMnLiHYRdCVKStByJtzMOu5vczADEYBbHSD5xnEgRr08yXsYkKA2dxxnSZ3OUNB4Ajb-J1QqNSqVTNosuQNsK5eONsF6CVQq65CyvRC7kIGOobBEIBpxsDMlssgSSXcsa8by8w9axBUSMUlUKaYceHYNghjR3KOMgMVpXOqQdbQv4mJt2SOEpMOwqtgunR5Kugo9u67DposYl-d3Sh9YPzlo8WKhs0UTLu5SQynUeeC2LzKEDqiNRgBS5J-fpJnIWpNMz3b9RAqmKoNCER81J3smRtGepv7Pver3L_N_Xu8eovfWOGraxDFAohNQOl4Sx7SykNaUCmAGQpikE1lfkVH1Je39lH9bbb9BL59dd_v_1g8dXHn05_PGwcPn77YOf_r4PiHc7X8q73Xl37j0e7aeDF7yU-fvPbzy-74SiM5c-K7z9IVZv8FdrvqmQ';
+    final Uint8List bytes = base64Url.decode(base64Url.normalize(sampleString));
+
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 20),
       child: Center(
         child: Card(
           child: Column(
             children: [
-              BarcodeWidget(
+              BarcodeWidget.fromBytes(
                 barcode: conf.barcode,
-                data: conf.normalizedData,
+                data: bytes,
                 width: conf.width,
                 height: conf.height,
                 style: GoogleFonts.sourceCodePro(
